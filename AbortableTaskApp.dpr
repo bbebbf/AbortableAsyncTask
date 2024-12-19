6b629bc69e6cb3ef1f0eb26324440f782ad723ea
Bernd Berends <bernd.berends@orgadata.com>
2024-12-19 07:27:48 +0100

    Projektdatei korrigiert

--- AbortableTaskApp.dpr
+++ AbortableTaskApp.dpr
@@ -2,13 +2,13 @@ program AbortableTaskApp;
 
 uses
   Vcl.Forms,
-  AbortableTask.Async.RunnerList in 'AbortableTask.Async.RunnerList.pas',
-  AbortableTask.Result in 'AbortableTask.Result.pas',
   AbortableTask.Async.Runner in 'AbortableTask.Async.Runner.pas',
+  AbortableTask.Async.RunnerList in 'AbortableTask.Async.RunnerList.pas',
   AbortableTask.Async.SharedData in 'AbortableTask.Async.SharedData.pas',
+  AbortableTask.Async.Worker in 'AbortableTask.Async.Worker.pas',
+  AbortableTask.Result in 'AbortableTask.Result.pas',
   AbortableTask.Tools in 'AbortableTask.Tools.pas',
   AbortableTask.Types in 'AbortableTask.Types.pas',
-  AbortableTask.Async.Worker in 'AbortableTask.Async.Worker.pas',
   ExampleTask in 'ExampleTask.pas',
   Helper.Async in 'Helper.Async.pas',
   unAbortableTask in 'unAbortableTask.pas' {fmMain},
@@ -19,7 +19,6 @@ uses
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TfmMain, fmMain);
   Application.CreateForm(TfmMain, fmMain);
   Application.Run;
 end.
